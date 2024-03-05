cc52050d5ff4cbe04b3bf24c0ea7cc8c6c3c4d93
wangchao <wangchao@golaxy.cn>
2024-03-05 16:05:01 +0800

    feat:打包类型文件处理

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import dts from 'vite-plugin-dts'
 import { fileURLToPath } from "node:url";
 import { resolve } from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     eslint(),
-    vueJsx()
+    vueJsx(),
+    dts({
+      tsconfigPath:'./tsconfig.build.json'
+    })
   ],
   resolve: {
     alias: {
