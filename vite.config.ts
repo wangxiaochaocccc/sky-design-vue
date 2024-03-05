8e79a88e888951f76732a5290f9bac088a383fd1
wangchao <wangchao@golaxy.cn>
2024-03-05 15:33:48 +0800

    feat:rollup打包配置and package.json配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import eslint from 'vite-plugin-eslint'
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import { fileURLToPath } from "node:url";
-
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -16,4 +16,20 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'TJDesignVue',
+      fileName:'tj-design-vue'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        exports: 'named',
+        globals: {
+          vue:'Vue'
+        }
+      }
+    }
+  }
 })
