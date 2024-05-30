197bf35cfb040f12ef9de4589ed1aa020ac7cd0f
wangchao <wangchao@golaxy.cn>
2024-05-30 15:26:39 +0800

    fixed:practice vitest API

--- src/components/button/button.test.ts
+++ src/components/button/button.test.ts
@@ -1,4 +1,4 @@
-import { describe,test } from 'vitest';
+import { describe,test,expect } from 'vitest';
 import { mount } from '@vue/test-utils'
 import Button from './Button.vue';
 
@@ -13,5 +13,29 @@ describe('Button.vue', () => {
       }
     })
     console.log(wrapper.html())
+    // test className
+    expect(wrapper.classes()).toContain('sky-button--primary')
+    // test solts
+    expect(wrapper.get('button').text()).toBe('button')
+    // test events
+    wrapper.get('button').trigger('click')
+    expect(wrapper.emitted()).toHaveProperty('click')
+  })
+
+  test('disabled', () => {
+    const wrapper = mount(Button, {
+      props: {
+        disabled:true,
+      },
+      slots: {
+        default: 'button',
+      }
+    })
+    // test disabled
+    expect(wrapper.get('button').element.disabled).toBeDefined()
+    expect(wrapper.attributes('disabled')).toBeDefined()
+    // test events 
+    wrapper.get('button').trigger('click')
+    expect(wrapper.emitted()).not.toHaveProperty('clcik')
   })
 })
