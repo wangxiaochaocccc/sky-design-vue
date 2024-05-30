f32f87c249dcc745ee3e779e45565d74075c4a07
wangchao <wangchao@golaxy.cn>
2024-05-30 16:02:53 +0800

    feat:button添加测试用例and use stub Api

--- src/components/button/button.test.ts
+++ src/components/button/button.test.ts
@@ -1,6 +1,8 @@
 import { describe,test,expect } from 'vitest';
 import { mount } from '@vue/test-utils'
 import Button from './Button.vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Icon from '../icon/icon.vue'
 
 describe('Button.vue', () => { 
   test('button', () => {
@@ -37,5 +39,44 @@ describe('Button.vue', () => {
     // test events 
     wrapper.get('button').trigger('click')
     expect(wrapper.emitted()).not.toHaveProperty('clcik')
+  })
+
+  test('icon', () => { 
+    const wrapper = mount(Button, {
+      props: {
+        icon:'arrow-up',
+      },
+      slots: {
+        default: 'icon',
+      },
+      global: {
+        stubs: ['FontAwesomeIcon']
+      }
+    })
+    console.log(wrapper.html())
+
+    const element = wrapper.findComponent(FontAwesomeIcon)
+    expect(element.exists()).toBe(true)
+    expect(element.attributes('icon')).toBe('arrow-up')
+  })
+
+  test('loading', () => { 
+    const wrapper = mount(Button, {
+      props: {
+        loading:true,
+      },
+      slots: {
+        default: 'icon',
+      },
+      global: {
+        stubs: ['Icon']
+      }
+    })
+    console.log(wrapper.html())
+
+    const element = wrapper.findComponent(Icon)
+    expect(element.exists()).toBe(true)
+    expect(element.attributes('icon')).toBe('spinner')
+    expect(wrapper.attributes('disabled')).toBeDefined()
   })
 })
